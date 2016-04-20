7b8848d04cd9c34285cb081ec9eacf8997d0283c
Knut Hühne <knut@k-nut.eu>
2016-04-20 22:19:25 +0200

    Enable filtering by opening hours

--- app.py
+++ app.py
@@ -1,23 +1,57 @@
 #/usr/bin/env python3
 import os
-from os import listdir
-from os.path import isfile, join
+import json
 
 from flask import Flask, request, jsonify
+
+from osm_time.opening_hours import OpeningHours
+
 app = Flask(__name__)
+
 
 def get_cities():
     filenames = [f for f in os.listdir("./data") if os.path.isfile(os.path.join("./data", f))]
     cities = [filename.split(".")[0] for filename in filenames]
     return cities
 
+
+def get_data_for(city):
+    filename = "%s.json" % city.lower()
+    path = os.path.join("./data", filename)
+    with open(path) as infile:
+        data = json.load(infile)
+        return data
+
+
+def filter_by(features, day, time):
+    filtered = []
+    for city in features:
+        opening_hours = city["properties"].get("opening_hours")
+        if opening_hours is None:
+            continue
+        openings = OpeningHours(opening_hours)
+        if openings.is_open(day, time):
+            filtered.append(city)
+    return filtered
+
 @app.route('/open_at')
 def open_at():
-    cities = get_cities
+    cities = get_cities()
     city = request.args.get('city', None)
-    if city is None or city.lower() not in cities:
-        return jsonify({"message": "city %s not found" % city})
-    return 'Hello World!'
+    if (city is None) or (city.lower() not in cities):
+        return json.dumps({"message": "city %s not found" % city}), 404
+    else:
+        data = get_data_for(city)
+
+    day = request.args.get("day", None)
+    time = request.args.get("time", None)
+
+    if day is not None and time is not None:
+        if day not in ["mo", "tu", "we", "th", "fr", "sa", "su"]:
+            return json.dumps({"message":"Parameter day must be one of [mo, tu, we, th, fr, sa, su]"}), 400
+        data["features"] = filter_by(data["features"], day, time)
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.debug = True
--- test_app.py
+++ test_app.py
@@ -4,14 +4,92 @@ import json
 
 import app
 
+TEST_DATA = {
+    "features": [
+        {
+            "geometry": {
+                "coordinates": [
+                    13.062124,
+                    52.401653
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "title": "Wochenmarkt Potsdam Bassinplatz",
+                "location": "Wochenmarkt Potsdam Bassinplatz",
+                "opening_hours": "Mo-Fr 07:00-16:00;Sa 07:00-13:00",
+            },
+            "type": "Feature"
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    13.095129,
+                    52.3942
+                ],
+                "type": "Point"
+            },
+            "properties": {
+                "title": "Floh- und Bauernmarkt in Potsdam Weberplatz, 14482 Potsdam",
+                "location": "Floh- und Bauernmarkt in Potsdam Weberplatz, 14482 Potsdam",
+                "opening_hours": "Sa 07:00-13:00",
+            },
+            "type": "Feature"
+        }
+    ]
+}
+
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = app.app.test_client()
+        app.app.config['TESTING'] = True
 
     @patch("app.get_cities", return_value=["berlin"])
     def test_city_does_not_exist(self, mock_get_cities):
         rv = self.app.get('/open_at?city=Hamburg')
-        print rv.data
+        assert rv.status_code == 404
         response = json.loads(rv.data)
         assert "not found" in response["message"]
+
+    @patch("app.get_cities", return_value=["berlin"])
+    @patch("app.get_data_for", return_value=TEST_DATA)
+    def test_returns_all_data_for_city_that_exsist(self, mock_get_data_for, mock_get_cities):
+        rv = self.app.get('/open_at?city=Berlin')
+        assert rv.status_code == 200
+        response = json.loads(rv.data)
+        assert len(response["features"]) == 2
+
+
+# If test_returns_all_data_for_city_that_exists and test_open_at_filters_by_opening_hours
+# are run from the same class one of them fails for some reason. Therefore they are
+# split into to test classes. This should not be neccessary though
+class SeparateTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.app.test_client()
+        app.app.config['TESTING'] = True
+
+    @patch("app.get_cities", return_value=["berlin"])
+    @patch("app.get_data_for", return_value=TEST_DATA)
+    def test_open_at_filters_by_opening_hours(self, mock_get_data_for, mock_get_cities):
+        rv = self.app.get('/open_at?city=Berlin&day=mo&time=14:00')
+        assert rv.status_code == 200
+        response = json.loads(rv.data)
+        assert len(response["features"]) == 1
+        
+class AnotherTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.app.test_client()
+        app.app.config['TESTING'] = True
+
+    @patch("app.get_cities", return_value=["berlin"])
+    @patch("app.get_data_for", return_value=TEST_DATA)
+    def test_validates_day_parameter(self, mock_get_data_for, mock_get_cities):
+        rv = self.app.get('/open_at?city=Berlin&day=no&time=12:00')
+        assert rv.status_code == 400
+        response = json.loads(rv.data)
+        assert response["message"] == "Parameter day must be one of [mo, tu, we, th, fr, sa, su]"
+
+
