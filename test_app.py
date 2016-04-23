0617ec9df7663dc4ee4f7e78f749a6f9406141df
Knut Hühne <knut@k-nut.eu>
2016-04-23 18:42:05 +0200

    Move all tets into one Class
    - removes the ugly workaround of creating multiple test classses
    - It seems like something was modifying the TEST_DATA in place.
      Always returning a fresh copy solves the problem

--- test_app.py
+++ test_app.py
@@ -5,40 +5,41 @@ from mock import patch
 
 import app
 
-TEST_DATA = {
-    "features": [
-        {
-            "geometry": {
-                "coordinates": [
-                    13.062124,
-                    52.401653
-                ],
-                "type": "Point"
+def create_fake_data():
+    return {
+        "features": [
+            {
+                "geometry": {
+                    "coordinates": [
+                        13.062124,
+                        52.401653
+                    ],
+                    "type": "Point"
+                },
+                "properties": {
+                    "title": "Wochenmarkt Potsdam Bassinplatz",
+                    "location": "Wochenmarkt Potsdam Bassinplatz",
+                    "opening_hours": "Mo-Fr 07:00-16:00;Sa 07:00-13:00",
+                },
+                "type": "Feature"
             },
-            "properties": {
-                "title": "Wochenmarkt Potsdam Bassinplatz",
-                "location": "Wochenmarkt Potsdam Bassinplatz",
-                "opening_hours": "Mo-Fr 07:00-16:00;Sa 07:00-13:00",
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    13.095129,
-                    52.3942
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "title": "Floh- und Bauernmarkt in Potsdam Weberplatz, 14482 Potsdam",
-                "location": "Floh- und Bauernmarkt in Potsdam Weberplatz, 14482 Potsdam",
-                "opening_hours": "Sa 07:00-13:00",
-            },
-            "type": "Feature"
-        }
-    ]
-}
+            {
+                "geometry": {
+                    "coordinates": [
+                        13.095129,
+                        52.3942
+                    ],
+                    "type": "Point"
+                },
+                "properties": {
+                    "title": "Floh- und Bauernmarkt in Potsdam Weberplatz, 14482 Potsdam",
+                    "location": "Floh- und Bauernmarkt in Potsdam Weberplatz, 14482 Potsdam",
+                    "opening_hours": "Sa 07:00-13:00",
+                },
+                "type": "Feature"
+            }
+        ]
+    }
 
 class FlaskrTestCase(unittest.TestCase):
 
@@ -63,7 +64,7 @@ class FlaskrTestCase(unittest.TestCase):
         assert "not found" in response["message"]
 
     @patch("app.controller.get_cities", return_value=["berlin"])
-    @patch("app.controller.get_data_for", return_value=TEST_DATA)
+    @patch("app.controller.get_data_for", return_value=create_fake_data())
     def test_returns_all_data_for_city_that_exsist(self, mock_get_data_for, mock_get_cities):
         response = self.app.get('/open_at?city=Berlin')
         assert response.status_code == 200
@@ -71,29 +72,17 @@ class FlaskrTestCase(unittest.TestCase):
         assert len(response["features"]) == 2
 
     @patch("app.controller.get_cities", return_value=["berlin"])
-    @patch("app.controller.get_data_for", return_value=TEST_DATA)
+    @patch("app.controller.get_data_for", return_value=create_fake_data())
     def test_validates_day_parameter(self, mock_get_data_for, mock_get_cities):
         response = self.app.get('/open_at?city=Berlin&day=no&time=12:00')
         assert response.status_code == 400
         response = json.loads(response.data)
         assert response["message"] == "Parameter day must be one of ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su']"
 
-
-
-# If test_returns_all_data_for_city_that_exists and test_open_at_filters_by_opening_hours
-# are run from the same class one of them fails for some reason. Therefore they are
-# split into to test classes. This should not be neccessary though
-class SeparateTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.app = app.app.test_client()
-        app.app.config['TESTING'] = True
-
     @patch("app.controller.get_cities", return_value=["berlin"])
-    @patch("app.controller.get_data_for", return_value=TEST_DATA)
+    @patch("app.controller.get_data_for", return_value=create_fake_data())
     def test_open_at_filters_by_opening_hours(self, mock_get_data_for, mock_get_cities):
         response = self.app.get('/open_at?city=Berlin&day=mo&time=14:00')
         assert response.status_code == 200
         response = json.loads(response.data)
         assert len(response["features"]) == 1
-
